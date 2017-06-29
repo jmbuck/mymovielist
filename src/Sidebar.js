d006d0c0c5a16397be721782b2fbf10e19b3a69f
Jordan Buckmaster <buckmast@purdue.edu>
2017-06-28 23:46:33 -0500

    Add navlinks to sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,14 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom'
 import './Sidebar.css';
 
-class Sidebar extends Component {
-  render() {
+const Sidebar = () => 
+{
     return (
       <div className="Sidebar">
-
+          <div className="nav">
+            <Link to="/new" className="new">
+              <button className="new">Add New</button>
+            </Link>
+              <div className="lists">
+                <Link to="/complete" className="complete">
+                  <button className="complete">Completed Movies</button>
+                </Link>
+                <Link to="/downloaded" className="downloaded">
+                  <button className="downloaded">Downloaded Movies</button>
+                </Link>
+                <Link to="/ptw" className="downloaded">
+                  <button className="ptw">Plan To Watch</button>
+                </Link>
+                <Link to="/dropped" className="dropped">
+                  <button className="dropped">Dropped Movies</button>
+                </Link>
+              </div>
+          </div>
       </div>
-    );
-  }
+    )
 }
 
 export default Sidebar;
